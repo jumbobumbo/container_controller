25b7011b8ea50104cf914cc6c21068c83e1e6a19
jumbobumbo <MasterCh0der!*>
2020-01-06 21:13:34 +0000

    plex, pi_hole py complete

---
+++ plex_pi_hole.py
@@ -0,0 +1,21 @@
+from common.config_reader import ReturnConfDict
+from common.enter_cmds import SendCmds as cmd
+
+
+# config data we are using
+config = ReturnConfDict("plex_pi_hole.py").json_data
+
+# check container status
+active_containers = cmd.return_cmd_response(config["generic_cmds"]["check_act_cont"])
+plex_status = config["containers"]["plex"]["container_name"] in active_containers
+ph_status = config["containers"]["pi_hole"]["container_name"] in active_containers
+
+if not plex_status or ph_status:
+    raise Exception(f"Plex up: {plex_status}, Pi hole up: {ph_status}")
+
+# pull latest images - restart containers
+for _ in [config["containers"]["plex"]["container_name"], config["containers"]["pi_hole"]["container_name"]]:
+    cmd(config["containers"][_]["dir"],
+        config["containers"][_]["pull"],
+        config["generic_cmds"]["down"],
+        config["generic_cmds"]["up"]).send_commands()
